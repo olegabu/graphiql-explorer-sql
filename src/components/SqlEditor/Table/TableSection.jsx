12bd0f92e7bf871347852e231cd8da610db41805
vasiliy.rogozha <vasiliy.rogozha@kindda.app>
2022-07-19 15:40:10 +0300

    added MaterialTable

--- src/components/SqlEditor/Table/TableSection.jsx
+++ src/components/SqlEditor/Table/TableSection.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import MaterialTable from 'material-table';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Search from '@material-ui/icons/Search';
 
 import { ReactComponent as LoaderSVG } from '../../../assets/imgs/loader.svg';
-import Table from './Table';
+import './Table.css';
 
 const TableSection = React.memo(({
   isFetching,
@@ -22,13 +30,27 @@ const TableSection = React.memo(({
     }
 
     return (
-      <section className="table-wrapper col-start-1 col-end-3 row-start-3 row-end-4 text-white my-12 overflow-hidden">
+      <section className="table-wrapper col-start-1 col-end-3 row-start-3 row-end-4 text-white overflow-hidden">
         {isFetching ? (
           <LoaderSVG className="w-20 mx-auto" />
         ) : (
-          <Table
+          <MaterialTable
+            title=""
+            icons={{
+              FirstPage,
+              LastPage,
+              Search,
+              ResetSearch: Clear,
+              PreviousPage: ChevronLeft,
+              NextPage: ChevronRight,
+              SortArrow: ArrowDownward,
+            }}
             columns={result ? result.columns : []}
             data={result ? result.data : []}
+            options={{
+              maxBodyHeight: '49vh',
+              doubleHorizontalScroll: true,
+            }}
           />
         )}
       </section>
